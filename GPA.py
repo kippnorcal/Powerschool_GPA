64af5c9eed3ff13718a882c67e0eda965a86cd99
Kiesha Xiao <kieshaxiao@RSO-FVHYK7GNJ1WK.local>
2019-07-17 10:14:11 -0700

    first draft of code

--- GPA.py
+++ GPA.py
@@ -3,29 +3,32 @@ import time
 import traceback
 from selenium import webdriver
 import bs4
-import pandas as pd
+import pandas as pdpi
 
 
 browser = webdriver.Firefox()
 
 def login():
-    browser.get(f"{os.getenv('PS_URL')}/pw.html") 
+    PS_URL = f"{os.getenv('PS_URL')}/pw.html"
+    browser.get(PS_URL) 
     user_field = browser.find_element_by_id('fieldUsername') 
     user_field.send_keys(os.getenv("USER")) 
     pwd_field = browser.find_element_by_id('fieldPassword') 
     pwd_field.send_keys(os.getenv("PWD")) 
     submit_button = browser.find_element_by_id('btnEnter')
     submit_button.click() 
-    time.sleep(1) 
+    time.sleep(5) 
 
 def search_bar():       
     search_bar = browser.find_element_by_id('studentSearchInput')
     search_bar.send_keys(os.getenv("SEARCH"))
-    search_bar.click() 
-    time.sleep(1)
+    search_button = browser.find_element_by_id('searchButton') 
+    search_button.click() 
+    time.sleep(5)
 
 def direct_quickExport(): 
-    browser.get(f"{os.getenv('QE_URL')}/importexport/exportstudents.html?dothisfor=selected")
+    QE_URL = f"{os.getenv('PS_URL')}/importexport/exportstudents.html?dothisfor=selected"
+    browser.get(QE_URL)
     text_box = browser.find_element_by_id('tt')
     text_box.send_keys(os.getenv("QUICK_EXPORT"))
     submit_btn = browser.find_element_by_id('btnSubmit')
@@ -33,10 +36,16 @@ def direct_quickExport():
     time.sleep(1)
 
 def main():
-        login() 
-
-        browser.quit() 
+        try:
+            login()
+            search_bar()
+            direct_quickExport()
+        
+        except Exception as e:
+            print(e)
+        finally:
+            browser.quit() 
 
 
 if __name__ == "__main__":
-    main() 
+        main() 
