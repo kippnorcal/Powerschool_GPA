9ef19b8e1b8f4e80d6e51c218037cd92d83e1406
dhess <dan.hess@kippbayarea.org>
2019-08-15 17:29:58 -0700

    Add transformations

--- data_map.py
+++ data_map.py
@@ -9,3 +9,10 @@ columns = {
     f'^(*gpa method="{method}" format=##0.00)': "Cumulative_GPA",
     '^(*gpa method="Weighted" format=##0.00 grade="9,10,11,12")': "Cumulative_Weighted_GPA",
 }
+
+column_names = {
+    key.replace("^(", "").replace(")", "").replace(" ", "_"): value
+    for key, value in columns.items()
+}
+
+keys = list(columns.keys())
